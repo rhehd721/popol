1c84882611c8eaeeb7c73b9096ac7a03fbb54968
알파카 <rhehd721@naver.com>
2021-04-20 20:40:59 +0900

    포트폴리오 사이트 만들기

---
+++ BottleHouse/PortFolio/static/js/home.js
@@ -0,0 +1,15 @@
+// function Skills_Click() {
+
+//     const digtal_clock = document.querySelector('.digital-clock');
+
+//     setInterval(() => {
+//         let date = new Date();
+//         digtal_clock.innerHTML = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+//     });
+// }
+
+// Skills_Click();
+
+function Skills_Click() {
+    document.getElementById("toggler").click();
+}
