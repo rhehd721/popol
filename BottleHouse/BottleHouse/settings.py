35e560b806126ee81000c203324d37abe7030a72
알파카 <rhehd721@naver.com>
2021-04-20 22:50:04 +0900

    포트폴리오 사이트 만들기

--- BottleHouse/BottleHouse/settings.py
+++ BottleHouse/BottleHouse/settings.py
@@ -103,13 +103,11 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
 
 # STATIC_DIR = os.path.join(BASE_DIR, 'APP_Name', 'static_folder_name')
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATIC_DIR = os.path.join(BASE_DIR, 'PortFolio',  'static')
 
 STATICFILES_DIRS = [
     STATIC_DIR,
@@ -117,3 +115,8 @@ STATICFILES_DIRS = [
 
 # static 파일들이 어디로 모일것인지
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+
+# media 파일들이 어디로 모일것인지
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- BottleHouse/PortFolio/urls.py
+++ BottleHouse/PortFolio/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path, include
 from . import views
+from django.conf import settings    # media
+from django.conf.urls.static import static    # media
+
 
 urlpatterns = [
     path('', views.enter, name = "enter"),
